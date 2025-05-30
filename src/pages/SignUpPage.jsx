1b09dec0f5e479f1f6a814c4d975dfc76b859077
Bhagirath Patel <112346654+bhagirath-bhp@users.noreply.github.com>
2025-05-30 13:01:14 +0530

    Update SignUpPage.jsx

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,11 +1,13 @@
-import { SignUp } from '@clerk/clerk-react'
+import { SignUp } from '@clerk/clerk-react';
 
 const SignUpPage = () => {
     return (
         <div className='w-full flex justify-center'>
-            <SignUp forceRedirectUrl="http://localhost:5173/chat"/>
+            <SignUp 
+                redirectUrl="/chat"
+            />
         </div>
-    )
+    );
 }
 
-export default SignUpPage
+export default SignUpPage;
